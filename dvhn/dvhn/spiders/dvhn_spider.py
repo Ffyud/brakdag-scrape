b8b711971193a8a9546c6cd92fac083fa3b67523
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-05-07 14:00:47 +0200

    toevoegen dvhn spider

--- dvhn/dvhn/spiders/dvhn_spider.py
+++ dvhn/dvhn/spiders/dvhn_spider.py
@@ -4,7 +4,10 @@ import scrapy
 class DvhnSpiderSpider(scrapy.Spider):
     name = 'dvhn_spider'
     allowed_domains = ['dvhn.nl']
-    start_urls = ['http://dvhn.nl/']
+    start_urls = ['https://www.dvhn.nl/groningen/stad/']
 
     def parse(self, response):
-        pass
+        title = response.xpath('//a[@class="default-hero__title-link"]/text()').get()
+        descr = response.xpath('//p[@class="default-hero__paragraph"]/text()').get()
+        link = response.xpath('//a[@class="default-hero__title-link"]/@href').get()
+        return {"title": title, "description": descr, "link": "https://dvhn.nl" + link}
